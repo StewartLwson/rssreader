63b0115cd035310ad54b2d9a5ca034f991e01890
Stewart Lawson <stewartlwson@gmail.com>
2019-03-05 07:58:13 +0000

    implemented input and validation

---
+++ classes/Input.php
@@ -0,0 +1,29 @@
+<?php // Class that handles input
+class Input {
+
+    // Determines what type of request is being made if it exists
+    public static function exists($type = 'post') {
+        switch($type) {
+            case 'post':
+                return (!empty($_POST)) ? true : false;
+            break;
+            case 'get':
+                return (!empty($_GET)) ? true : false;
+            break;
+            default:
+                return false;
+            break;
+        }
+    }
+
+    // Returns the item sent in the request
+    public static function get($item) {
+        if(isset($_POST[$item])) {
+            return $_POST[$item];
+        } else if(isset($_GET[$item])) {
+            return $_GET[$item];
+        }
+        return '';
+    }
+}
+?>
---
+++ register.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'core/init.php';
+
+if(Input::exists()) {
+    $validate = new Validate();
+    $validation = $validate->check($_POST, array(
+        'username' => array(
+            'required' => true,
+            'min' => 2,
+            'max' => 20,
+            'unique' => 'users'
+        ),
+        'password' => array(
+            'required' => true,
+            'min' => 6
+        ),
+        'password_verify' => array(
+            'required' => true,
+            'matches' => 'password'
+        ),
+        'name' => array(
+            'required' => true,
+            'min' => 2,
+            'max' => 50
+        )
+    ));
+
+    if ($validate->passed()) {
+        echo "Passed";
+    } else {
+        foreach($validation->errors() as $error) {
+            echo $error, "<br>";
+        }
+    }
+}
+?>
+
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" value="<?php echo escape(Input::get("username"));?>" autocomplete="off">
+    </div>
+    <div class="field">
+        <label for="password">Choose a password</label>
+        <input type="password" name="password" id="password" value="" autocomplete="off">
+    </div>
+    <div class="field">
+        <label for="password_verify">Verify Password</label>
+        <input type="password" name="password_verify" id="password_verify" value="" autocomplete="off">
+    </div>
+    <div class="field">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" value="<?php echo escape(Input::get("name"));?>" autocomplete="off">
+    </div>
+
+    <input type="submit" value="Register">
+</form>
